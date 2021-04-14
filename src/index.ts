181a7605fb273f9deed4d7de1194f5c42f9f3a6e
KacperM717 <kacpermikolajczak10@gmail.com>
2021-04-14 13:15:59 +0200

    Nodemailer setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import './util/secrets';
+import http from 'http';
 import app from './app';
 
 if (process.env.ENV_MODE === 'dev') {
   console.log('In development mode');
 }
 
-const server = app.listen(app.get('port'), () => {
+// Passing express to http - makes it available for socket.io and https in future
+const server = http.createServer(app);
+
+server.listen(app.get('port'), () => {
   console.log(`App is running on port ${app.get('port')}`);
 });
 
