afcb6410b0c5dcba58906474c9cac0b1812284b8
KacperM717 <kacpermikolajczak10@gmail.com>
2021-05-01 00:54:01 +0200

    Missing video event recovered

--- src/sockets/video.socket.ts
+++ src/sockets/video.socket.ts
@@ -1,6 +1,10 @@
 import { UserSocket } from '../types';
 
 export const attachVideo = (socket: UserSocket) => {
+  socket.on('video:call', (friendId) => {
+    console.log(`${socket.user?._id} is calling ${friendId}`);
+    socket.to(friendId).emit('video:calling', socket.user?._id);
+  });
   socket.on('video:reject', (friendId) => {
     socket.to(friendId).emit('video:rejected', socket.user?._id);
   });
